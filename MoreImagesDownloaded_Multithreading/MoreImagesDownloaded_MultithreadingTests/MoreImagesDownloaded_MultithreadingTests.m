d34257dc0c0c458fc91a60fc99ef26739ee4857d
susu <28025920@qq.com>
2016-05-07 14:29:09 +0800

    创建了项目

---
+++ MoreImagesDownloaded_Multithreading/MoreImagesDownloaded_MultithreadingTests/MoreImagesDownloaded_MultithreadingTests.m
@@ -0,0 +1,39 @@
+//
+//  MoreImagesDownloaded_MultithreadingTests.m
+//  MoreImagesDownloaded_MultithreadingTests
+//
+//  Created by  江苏 on 16/5/7.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoreImagesDownloaded_MultithreadingTests : XCTestCase
+
+@end
+
+@implementation MoreImagesDownloaded_MultithreadingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
